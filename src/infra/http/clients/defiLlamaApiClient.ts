d2458696bebe291a901e5c8df1906bb3cb546385
Anderson Begossi <andersonbegossi@gmail.com>
2025-12-09 21:21:24 -0300

    feat: add http defiLlamaClient

---
+++ src/infra/http/clients/defiLlamaApiClient.ts
@@ -0,0 +1,10 @@
+import { env } from "@/config/env";
+import axios from "axios";
+
+export const defiLlamaClient = axios.create({
+  baseURL: env.EXPO_PUBLIC_DEFILLAMA_BASE_API_URL,
+  timeout: 15000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
---
+++ src/infra/http/clients/index.ts
@@ -0,0 +1,1 @@
+export { defiLlamaClient } from "./defiLlamaApiClient";
