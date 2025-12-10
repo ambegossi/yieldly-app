984c820550d8324f95faea120a5123e48eabcfbd
Anderson Begossi <andersonbegossi@gmail.com>
2025-12-09 21:32:59 -0300

    feat: add query client provider and repository provider on RootLayout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,21 @@
+import { createRepositories } from "@/infra/container";
+import { RepositoryProvider } from "@/infra/repositories/RepositoryProvider";
 import { PortalHost } from "@rn-primitives/portal";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import "../../global.css";
 
+const queryClient = new QueryClient();
+
+const repositories = createRepositories();
+
 export default function RootLayout() {
   return (
-    <>
-      <Stack />
-      <PortalHost />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <RepositoryProvider value={repositories}>
+        <Stack />
+        <PortalHost />
+      </RepositoryProvider>
+    </QueryClientProvider>
   );
 }
